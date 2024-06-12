b02b6e1a8d2f97765080480d46e8af9e4513f84a
shuravi <ravi.official097@gmail.com>
2024-06-12 22:22:30 +0530

    a

--- src/app/service/hardcoded-authentication.service.ts
+++ src/app/service/hardcoded-authentication.service.ts
@@ -7,6 +7,9 @@ export class HardcodedAuthenticationService {
   constructor() {}
 
   authenticate(username: string, password: string): boolean {
+    if (sessionStorage.getItem('authenticaterUser') != null) {
+      return true;
+    }
     if (username === 'ravi' && password === 'shubhashini') {
       sessionStorage.setItem('authenticaterUser', username);
       return true;
